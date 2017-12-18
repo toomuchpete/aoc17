5051ff84edd3f0d157f73c623399313b9e535952
Pete Holiday <pete.holiday@gmail.com>
2017-12-18 00:49:00 -0500

    Day 18: Star 2

--- day_18.php
+++ day_18.php
@@ -18,58 +18,73 @@ $input = trim(file_get_contents('./data/day_18.txt'));
 $instructions = explode("\n", $input);
 
 $pointer = 0;
-$reg     = [];
-$sounds  = [];
+$reg = [
+    ['p' => 0],
+    ['p' => 1]
+];
 
-while ($pointer < count($instructions)) {
+$ptr = [0, 0];
+
+$q = [[],[]];
+$proc = 0;
+
+$send_count = [0,0];
+
+$wait = [false, false];
+
+while (true) {
     $move = 1;
-    $ins  = explode(' ', $instructions[$pointer]);
+    $ins  = explode(' ', $instructions[$ptr[$proc]]);
 
-    echo "> {$instructions[$pointer]}\n";
+    $other_proc = ($proc+1) % 2;
 
-    
     switch ($ins[0]) {
         case 'snd':
-            $freq = get_val($ins[1]);
-            $sounds[] = $freq;
-            echo " - Sound Played: {$freq}\n";
+            $q[$other_proc][] = get_val($ins[1]);
+            $send_count[$proc]++;
             break;
         case 'set':
-            $reg[$ins[1]] = get_val($ins[2]);
+            $reg[$proc][$ins[1]] = get_val($ins[2]);
             break;
         case 'add':
-            $reg[$ins[1]] += get_val($ins[2]);
+            $reg[$proc][$ins[1]] += get_val($ins[2]);
             break;
         case 'mul':
-            $reg[$ins[1]] *= get_val($ins[2]);
+            $reg[$proc][$ins[1]] *= get_val($ins[2]);
             break;
         case 'mod':
-            $reg[$ins[1]] = $reg[$ins[1]] % get_val($ins[2]);
+            $reg[$proc][$ins[1]] = $reg[$proc][$ins[1]] % get_val($ins[2]);
             break;
         case 'rcv':
-            if (get_val($ins[1]) != 0) {
-                echo " - Sound Recovered: " . end($sounds) . "\n";
-                break 2;
+            $wait[$proc] = false;
+            if (empty($q[$proc]) == false) {
+                $reg[$proc][$ins[1]] = array_shift($q[$proc]);
+            } else {
+                $wait[$proc] = true;
+                $move = 0;
             }
             break;
         case 'jgz':
             if (get_val($ins[1]) > 0) {
-                $move = $ins[2];
+                $move = get_val($ins[2]);
             }
             break;
     }
 
-    $pointer += $move;
+    if ($wait[0] && $wait[1]) { break; } // Deadlocked
+
+    $ptr[$proc] += $move;
+    $proc = $other_proc;
 }
 
-print_r($sounds);
+print_r($send_count);
 
 function get_val($name) {
-    global $reg;
+    global $reg, $proc;
 
     if (is_numeric($name)) { return $name; };
 
-    if (isset($reg[$name]) == false) { $reg[$name] = 0; }
+    if (isset($reg[$proc][$name]) == false) { $reg[$proc][$name] = 0; }
 
-    return $reg[$name];
+    return $reg[$proc][$name];
 }
